6f847d42a0b22098e09b045bf0f6320c2b1c1c7b
gollavilli ganesh kumar <84216653+ganesh2328@users.noreply.github.com>
2021-08-07 10:41:21 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./App.css";
+import Circle from "./Circle.js";
+
+export default function App() {
+  let [count, setCount] = useState([
+    {
+      id: "red",
+      count: 0
+    },
+    {
+      id: "blue",
+      count: 0
+    },
+    {
+      id: "green",
+      count: 0
+    }
+  ]);
+
+  let increment = id => {
+    let countIndex = count.findIndex(obj => obj.id === id);
+    count[countIndex].count++;
+    setCount([...count]);
+  };
+  let reset = () => {
+    count.map(obj => {
+      obj.count = 0;
+    });
+    setCount([...count]);
+  };
+
+  return (
+    <div >
+     <div className="App1">
+      <button onClick={reset} className="btn1">Reset</button></div>
+     <div className="App">
+      {count.map(obj => {
+        return <Circle data={obj} handleCount={increment} className="App" />;
+       
+      })}
+       </div>
+    </div>
+  );
+}
---
+++ src/Circle.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Circle(props) {
+    const {id} = props.data
+
+    return (
+        <div>
+            <h2>{props.data.count}</h2>
+            <button onClick = { ()  => { props.handleCount (props.data.id); }} className = "btn" style={{backgroundColor: id}}> click </button>
+            
+            
+        </div >
+    );
+
+}
