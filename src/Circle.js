3692a20a7856f88c0d1ff40fa0098d765b9123d9
gollavilli ganesh kumar <84216653+ganesh2328@users.noreply.github.com>
2021-08-07 11:04:05 +0530

    second commit

--- src/Circle.js
+++ src/Circle.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React from "react";
 
 export default function Circle(props) {
-    const {id} = props.data
+  const { id } = props.data;
 
-    return (
-        <div>
-            <h2>{props.data.count}</h2>
-            <button onClick = { ()  => { props.handleCount (props.data.id); }} className = "btn" style={{backgroundColor: id}}> click </button>
-            
-            
-        </div >
-    );
-
+  return (
+    <div>
+      <h2>{props.data.count}</h2>
+      <button
+        onClick={() => {
+          props.handleCount(props.data.id);
+        }}
+        className="btn"
+        style={{ backgroundColor: id }}
+      >
+        click
+      </button>
+    </div>
+  );
 }
